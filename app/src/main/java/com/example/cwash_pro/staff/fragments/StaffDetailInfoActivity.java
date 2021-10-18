e27f7f152f2b08ab074390032f6c3048cc962254
longtq <longcq@its-global.vn>
2021-10-18 22:18:08 +0700

    Clone UI old

--- app/src/main/java/com/example/cwash_pro/staff/fragments/StaffDetailInfoActivity.java
+++ app/src/main/java/com/example/cwash_pro/staff/fragments/StaffDetailInfoActivity.java
@@ -61,6 +61,6 @@ public class StaffDetailInfoActivity extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_staff_detail_info_activity, container, false);
+        return inflater.inflate(R.layout.activity_staff_detail_info, container, false);
     }
 }
